a760712d7dcf2b1a9afc3ccd2f8bb954fed84eb9
mathhun <achemer@gmail.com>
2015-08-14 23:03:20 +0900

    Initial commit

---
+++ ruby/ulam.rb
@@ -0,0 +1,43 @@
+require 'rmagick'
+require 'prime'
+
+N = 50
+W = 10
+
+Background = '#F0F0F0'
+Foregrand = '#86D360'
+
+# stolen from http://rosettacode.org/wiki/Ulam_spiral_(for_primes)#Ruby
+def cell(x, y, start=1)
+  y, x = y - N/2, x - (N - 1)/2
+  l = 2 * [x.abs, y.abs].max
+  d = y >= x ? l*3 + x + y : l - x - y
+  (l - 1)**2 + d + start - 1
+end
+
+def main
+  canvas = Magick::Image.new(N*W, N*W) { self.background_color = Background }
+  gc = Magick::Draw.new
+  gc.fill(Foregrand)
+
+  N.times do |y|
+    N.times do |x|
+      n = cell(x, y)
+
+      if n.prime?
+        xx = x * W
+        yy = y * W
+        gc.polygon(xx, yy,
+                   xx + W, yy,
+                   xx + W, yy + W,
+                   xx, yy + W)
+      end
+    end
+  end
+
+  gc.draw(canvas)
+
+  canvas.write("ulam#{N}.png")
+end
+
+main
